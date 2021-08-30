cff98f91b28f7775897047c93fd0a815d1a35ea8
guoyansai <guoyansai@qq.com>
2021-08-30 19:46:58 +0800

    更新文件夹位置

---
+++ xm.js
@@ -0,0 +1,91 @@
+var xmData = [
+  {
+    tit: "华为云服务线多款产品前端开发",
+    des: "华为主题设计工具：基于electron/vue/nodejs架设开发环境，通过js/sass/svg/canvas/i18n等技术开发win/Mac全端全球适配的IDE；华为创意广告设计平台：熟练运用vue/es6/axios/webpack等技术部署开发与发布环境，制作在线版图片、H5页面编辑器；华为PPS广告投放系统：比肩GoogleAds的广告投放、创意设计系统、商品酒店关键词投放等。",
+    key: "js,css,html,nodejs,vue,electron,sass,less,svg,canvas,i18n,webpack,es6,elementUI,axios,angular,ts,讲师,培训,导师,架构师,项目管理,命题,面试",
+    com: "软通动力信息技术（集团）股份有限公司",
+    wok: "技术架构：前端项目技术选型、架构设计、开发启动与升级维护等；项目管理：负责前端团队的开发总调；新员工培训导师：每月一次的新员工入职技术培训；前端技能鉴定组长：每年两次的前端技能鉴定组组长、命题人；另有前端面试、代码review、安全检视等。",
+    oth: "2019.7-至今*",
+  },
+  {
+    tit: "自由工作接单开发各种类型网站及手机软件开发",
+    des: "生物医药科技行业网站：需求分析、网站架构、前端页面设计、UX交互设计、设计开发、上线运营与维护等全部技术工作；二手车检测服务平台：市场调研、需求分析、网站架构，并独立完成网站设计与开发；专用车电子商务平台：平台的架构、数据库、WEB前端设计、程序开发、上线部署等，独立完成全部开发工作；车商记账统计服务：使用VUE框架、uni-app进行APP前端开发，同时制作官方网站及相关宣传页等。",
+    key: "js,css,html,bootstrap,jq,php,mysql,asp,mssql,vue,uniapp,es6,nodejs,less,canvas,团队协作",
+    com: "南京车商记信息科技有限公司",
+    wok: "自由开发者：与朋友合伙接单各种平台、网站、软件等需求开发。",
+    oth: "2018.12-2019.6",
+  },
+  {
+    tit: "生产设备电子商务平台",
+    des: "中国设备联盟(技术入股)：带领团队完成旗下十二个行业网站的前端改版升级、底层技术重构及平台维护与运营工作。",
+    key: "js,css,html,asp,mssql,vue,bootstrap,jq,管理,运维,服务器,安全,数据库",
+    com: "南京搜浪互联网络技术有限公司",
+    wok: "技术总监：定期培训，平台计划性重构与升级开发，解决数据查询优化、服务器安全等问题，日常化运维与新需求计划制定。",
+    oth: "2009.2-2018.11",
+  },
+  {
+    tit: "汽车交易服务平台",
+    des: "南京二手车网、新车网、租车网：负责旧站改版，并完成新车网、租车网及其他特需项目的前端设计、后端数据库与程序开发。",
+    key: "js,css,html,asp,mssql,技术架构,网站重构,技术管理",
+    com: "南京搜车科技股份有限公司",
+    wok: "技术主管：旧版二手车网站的重构与优化，新车网、租车网的设计开发到上线。",
+    oth: "2008.2-2009.1",
+  },
+  {
+    tit: "电子元器件资讯网",
+    des: "SMD资讯网，辞去教师工作来到南京后的第一个网站项目，含资讯、论坛、商品展示等。",
+    key: "js,css,html,asp,mssql,网站开发,网页设计",
+    com: "南京南山半导体有限公司",
+    wok: "网站开发：公司网站设计与开发，SMD行业资讯网站的前后端开发。",
+    oth: "2007.7-2008.1",
+  },
+  {
+    tit: "中学网站CMS系统",
+    des: "完成在职学校官网、在职学科社区等一系列学校网络服务系统的开发。",
+    key: "js,css,html,asp,access,mssql,网络管理,电脑硬件,多媒体教学,校园网开发,学科网开发",
+    com: "江苏泗洪上塘中学",
+    wok: "任职生物教师、物理教师、计算机管理员，并负责学校网络建设及机房管理。",
+    oth: "2005.9-2007.6",
+  },
+  {
+    tit: "精品课程网站",
+    des: "植物生理学网络课程建设，根据大学导师需求，从事教学素材收集、flash动画制作，以及网络课程、社区论坛、在线考试等综合功能网站开发。",
+    key: "js,css,html,ppt,flash,asp,access,课件制作,网站架构,网站运维,网站开发,考试系统",
+    com: "江苏师范大学",
+    wok: "大学时代：兴趣爱好网站建设，特别自学计算机相关知识，毕业论文是精品课程建设，并获得院系第一名，学校优秀毕业论文。",
+    oth: "2002.9-2005.6",
+  },
+  {
+    tit: "自研作品",
+    des: "自学提升作品：阿赛企业、门户、学校网站系统；资讯app、小程序；uniapp+php/mysql全端开发；vue+webpack开发的内容管理CMS；啾噗app等等。",
+    key: "js,css,html,angular,ts,nodejs,sass,i18n,webpack,vue,es6,AS,VS,VScode,DW,PS,VPN,GIT,SVN,前端,后端,项目架构,项目管理,项目运维,服务器,数据库,域名,备案,SEO",
+    com: "阿赛工作室",
+    wok: "业余时间：不断学习最新技术，开发分享开源项目，钻研改善公共模块，涉足网络服务方方面面，扩增知识面。",
+    oth: "2001.9-至今*",
+  },
+];
+
+var xmDom =
+  '<style>.xm-ol ol i{font-style:normal;margin:0 2px;padding:0 2px;border-radius:6px;background-color:#FC0;}.xm-ol ol{text-align:left;word-wrap:break-word;list-style:none;padding: 0px;margin: 0px;}.xm-ol ol{counter-reset: show-list;}.xm-ol ol li{padding-left: 30px;position: relative;counter-increment: item;}.xm-ol ol li:before{content: "("counter(item)")";display: block;position: absolute;left: 0;top:0;}.xm-ol ol u{color:#888;}</style><div class="xm-ol"><ol>';
+var xmDataLen = xmData.length;
+var xmi = 0;
+var xmItem;
+var xmKeys = [];
+var xmKeysLen = 0;
+var xmKeyi = 0;
+while (xmi < xmDataLen) {
+  xmItem = xmData[xmi];
+  xmKeys = xmItem.key.split(",");
+  xmKeysLen = xmKeys.length;
+  xmKeyi = 0;
+  xmDom += "<li>【" + xmItem.tit + "】：" + xmItem.des + "<u>" + xmItem.wok + "</u>[" + xmItem.com + "](" + xmItem.oth + ")";
+  while (xmKeyi < xmKeysLen) {
+    xmDom += "<i>" + xmKeys[xmKeyi] + "</i>";
+    xmKeyi++;
+  }
+  xmDom += "</li>";
+  xmi++;
+}
+xmDom += "</ol></div>";
+
+document.write(xmDom);
